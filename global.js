4c559216a2e353cb4486f6855abb9a8e8cdafb2b
seanreibling <seanreibling@outlook.com>
2024-04-22 20:27:53 -0400

    V1.15

--- global.js
+++ global.js
@@ -1,20 +1,71 @@
-console.log("V1.14");
+console.log("V1.15");
 
 
 const swup = new Swup({
-    // plugins: [new SwupProgressPlugin()]
+  // plugins: [new SwupProgressPlugin()]
+});
+
+swup.hooks.on('page:view', () => {
+  // This runs after every page change
+  // example();  
+});
+
+swup.hooks.on('visit:end', () => {
+  // This runs after each Swup transition completes
+  console.log('Transition completed. Initializing slideshows...');
+  // example();
+});
+
+
+// Define custom transition function with delay
+function customTransition() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000); // 1 second delay
   });
-  
-  swup.hooks.on('page:view', () => {
-    // This runs after every page change
-    // example();  
-  });
-  
-  swup.hooks.on('visit:end', () => {
-    // This runs after each Swup transition completes
-    console.log('Transition completed. Initializing slideshows...');
-    // example();
-  });
+}
+
+// Listen for 'click' event on links
+document.addEventListener('click', (event) => {
+  const target = event.target;
+
+  // Check if the clicked element has the specific class name
+  if (target.classList.contains('transition-delay')) {
+    // Apply custom transition
+    swup.setTransition(customTransition);
+  } else {
+    // If not, use the default transition
+    swup.setTransition(swup.defaultTransitions);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Scroll Enable/Disable
@@ -31,49 +82,49 @@ function scrollEnable() {
   window.scrollTo(0, scrollPos);
 }
 
- document.getElementById('menu-open').addEventListener('click', function () {
+document.getElementById('menu-open').addEventListener('click', function () {
   scrollDisable();
- });
+});
 
- document.getElementById('menu-close').addEventListener('click', function () {
+document.getElementById('menu-close').addEventListener('click', function () {
   scrollEnable();
-  setTimeout(function() {
+  setTimeout(function () {
     document.getElementById('subpage-1').scrollTop = 0;
     document.getElementById('subpage-2').scrollTop = 0;
   }, 500);
- });
+});
 
- document.getElementById('return-1').addEventListener('click', function () {
-  setTimeout(function() {
+document.getElementById('return-1').addEventListener('click', function () {
+  setTimeout(function () {
     document.getElementById('subpage-1').scrollTop = 0;
   }, 250);
- });
+});
 
- document.getElementById('return-2').addEventListener('click', function () {
-  setTimeout(function() {
+document.getElementById('return-2').addEventListener('click', function () {
+  setTimeout(function () {
     document.getElementById('subpage-2').scrollTop = 0;
   }, 250);
- });
-  
+});
 
 
 
 
 
-  
-  
-  
-  
-  
-  //Close nav menu if device width is greater than 479px
-  
-  function checkWindowWidth() {
-    const windowWidth = window.innerWidth;
-  
-    if (windowWidth > 479) {
-      document.getElementById('menu-close').click();
-    }
+
+
+
+
+
+
+//Close nav menu if device width is greater than 479px
+
+function checkWindowWidth() {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth > 479) {
+    document.getElementById('menu-close').click();
   }
-  
-  window.addEventListener('resize', checkWindowWidth);
+}
+
+window.addEventListener('resize', checkWindowWidth);
 
