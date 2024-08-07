3b2fe445d36e157544d191d4842eaee604da6be7
seanreibling <29679089+seanreibling@users.noreply.github.com>
2024-08-07 17:13:38 -0400

    V1.193

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-console.log("V1.192");
+console.log("V1.193");
 
 //----PAGE TRANSITION FUNCTIONALITY----
 
@@ -1109,9 +1109,9 @@ function initCarousel() {
   const leftButton = document.getElementById('carousel-left');
   const rightButton = document.getElementById('carousel-right');
 
-  if (!carousel || !container || !leftButton || !rightButton) {
-    return; // Exit if any of the elements are not found
-  }
+  // if (!carousel || !container || !leftButton || !rightButton) {
+  //   return; // Exit if any of the elements are not found
+  // }
 
   const cardWidth = document.querySelector('.carousel-card').offsetWidth;
   const gap = parseInt(window.getComputedStyle(carousel).gap);
